cda87aebd62cd76622552ed70b2cc588b77b8087
FaayzFx <rafaelgam3scanal@gmail.com>
2025-10-13 11:33:11 -0300

    textos e categorias atualizadas

--- js/categories.js
+++ js/categories.js
@@ -4,59 +4,59 @@ const categories = [
   {
     title: "Tendências",
     text:
-      "Está precisando de novas indicações para a sua loja?<br><br>Aqui na Wilson, você encontra diversos modelos no atacado. Aproveite para se preparar com as <span class='paragraph-highlight'>melhores tendências e aumentar a sua margem de lucro.</span>",
+      "Está planejando seu estoque para a <span class='paragraph-highlight'> Black Friday?</span> <br><br> Na Distribuidora Wilson, você encontra os <span class='paragraph-highlight'>lançamentos e tendências</span> que impulsionam as vendas e mantêm sua loja <span class='paragraph-highlight'>sempre atualizada</span> com o que o consumidor procura.",
     image: "https://i.imgur.com/GzMzLns.png",
     link: "https://www.wilsonatacado.com.br/lancamentos-calcados-atacado.html"
   },
   {
     title: "Tênis & Botas",
     text:
-      "A coleção indispensável para o inverno inclui o mix de tênis e botas, entre elas temos <span class='paragraph-highlight'>promoções</span>.<br><br>Portanto, a <span class='paragraph-highlight'>melhor opção</span> é se preparar com marcas renomadas de calçados fechados para <span class='paragraph-highlight'>aquecer suas vendas nesta estação.</span>",
+      "Ténis e botas são <span class='paragraph-highlight'>escolhas certeiras</span> para o período de maior movimento do varejo. <br><br> Garanta um mix estratégico de calçados fechados para o seu estoque e mantenha sua loja pronta para atender à <span class='paragraph-highlight'> demanda da temporada.</span>",
     image: "https://i.imgur.com/q8ZtaxD.png",
     link: "https://www.wilsonatacado.com.br/colecao-de-inverno-atacado.html"
   },
   {
     title: "Promoções",
     text:
-      "<span class='paragraph-highlight'>Invista menos e lucre mais</span> com nossa categoria de <span class='paragraph-highlight'>Promoções de Calçados</span> no Atacado.<br><br>Esta é a oportunidade ideal para lojistas que desejam maximizar seus ganhos e oferecer <span class='paragraph-highlight'>produtos de qualidade a preços competitivos.</span>",
+      "Para quem busca ampliar o mix de produtos com investimento inteligente, <span class='paragraph-highlight'>nossa categoria de oportunidades é ideal.</span><br><br> Planeje-se com antecedência e garanta variedade de modelos com <span class='paragraph-highlight'>excelente potencial de venda no atacado.</span>",
     image: "https://i.imgur.com/Ath9Mgm.png",
     link: "https://www.wilsonatacado.com.br/outlet-calcados-atacado.html"
   },
   {
     title: "Infantil",
     text:
-      "Se você é um revendedor que busca encantar pais e responsáveis com produtos de qualidade, <span class='paragraph-highlight'>nosso catálogo de calçados infantis no atacado é a resposta.</span><br><br>Abasteça seu estoque!",
+      "O segmento infantil movimenta vendas o ano todo — <span class='paragraph-highlight'>e na Black Friday, não é diferente.</span> <br><br> Monte um estoque completo e atrativo com calçados que encantam pais e crianças, garantindo <span class='paragraph-highlight'>variedade e qualidade na sua vitrine.</span>",
     image: "https://i.imgur.com/fmPGNd8.png",
     link: "https://www.wilsonatacado.com.br/infantil.html"
   },
   {
     title: "Masculino",
     text:
-      "Destaque-se na moda masculina com as últimas tendências em calçados. <span class='paragraph-highlight'>Aumente suas vendas</span> e transforme cada passo em um símbolo de estilo.<br><br>Entre no mundo dos calçados masculinos no atacado e <span class='paragraph-highlight'>ofereça aos seus clientes uma experiência única.</span> Compre no atacado!",
+      "Prepare-se para atender o público masculino com <span class='paragraph-highlight'>estilo e estratégia.</span> <br><br> A categoria de calçados masculinos traz modelos versáteis e modernos, perfeitos para quem busca unir <span class='paragraph-highlight'>conforto, tendência e boa margem de venda.</span>",
     image: "https://i.imgur.com/yeMAXRl.png",
     link: "https://www.wilsonatacado.com.br/calcados-masculino-atacado.html"
   },
   {
     title: "Feminino",
     text:
-      "Nossa coleção é mais do que simplesmente sapatos; <span class='paragraph-highlight'>são expressões de estilo e personalidade.</span><br><br>Cada par é escolhido para elevar a confiança da mulher moderna, proporcionando <span class='paragraph-highlight'>conforto e elegância</span> em cada passo. Adquira já no atacado!",
+      "Os calçados femininos continuam entre os produtos <span class='paragraph-highlight'>mais desejados pelos consumidores.</span><br><br> Aposte em modelos que traduzem <span class='paragraph-highlight'>estilo, conforto e autenticidade</span> — e destaque sua loja com um mix pensado para encantar em cada detalhe.",
     image: "https://i.imgur.com/rovCT7u.png",
     link: "https://www.wilsonatacado.com.br/calcados-feminino-atacado.html"
   }
 ];
 
 const casePromoSlider = `
-  <swiper-container
-    class="promo-slider-cont"
-    slides-per-view="auto"
-    centered-slides="true"
-    grab-cursor="true"
-    slide-to-clicked-slide="true"
-    pagination="true"
-    pagination-clickable="true"
-    loop="false">
+              <swiper-container
+                class="promo-slider-cont"
+                slides-per-view="auto"
+                centered-slides="true"
+                grab-cursor="true"
+                slide-to-clicked-slide="true"
+                pagination="true"
+                pagination-clickable="true"
+                loop="false">
 
-    ${categories
+                ${categories
     .map(
       category => `
           <swiper-slide class="case-slider">
@@ -79,7 +79,7 @@ const casePromoSlider = `
     )
     .join('')
   }
-  </swiper-container>
-`;
+              </swiper-container>
+              `;
 
 promoSliderCont.innerHTML = casePromoSlider;
